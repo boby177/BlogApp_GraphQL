4828a371789c456499827160fe6d6b9830752af3
boby177 <bomasu37@gmail.com>
2022-12-26 13:01:28 +0700

    created login pages and connecting from api

---
+++ src/pages/login.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
+import { useLoginMutation } from "../gql/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          console.log(response);
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            // Data Valid
+            window.alert("Successfully Login");
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Box mt={4}>
+              <InputField
+                name="username"
+                placeholder="username"
+                label="username"
+              />
+              <InputField
+                name="password"
+                placeholder="password"
+                label="password"
+                type="password"
+              />
+            </Box>
+            <Button
+              type="submit"
+              colorScheme="teal"
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
